36c513ba7e3be0a8124066e71a11fabea034d534
bryant52021002 <149552674+bryant52021002@users.noreply.github.com>
2024-04-02 12:43:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const HOST = '0.0.0.0'
 const app = express()
 app.get('/', (req, res) => {
     res.send('bryant lie\n')
+    res.send('52021002\n')
+    res.send('Teknik Informatika\n')
 })
 
 app.listen(PORT, HOST)
