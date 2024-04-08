64b9e4bad16e62d042a45d51b104cde3f8277d91
bryant52021002 <149552674+bryant52021002@users.noreply.github.com>
2024-04-08 17:21:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const express = require('express');
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
+const facebookProfileLink = 'https://www.facebook.com/bryant.lie.73'; // Link ke profil Facebook
+
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Nama: Bryant Lie\nNIM: 52021002\n Jurusan: Teknik Informatika\n');
+    res.send(`Nama: Bryant Lie\nNIM: 52021002\nJurusan: Teknik Informatika\nProfil Facebook: <a href="${facebookProfileLink}">${facebookProfileLink}</a>`);
 });
 
 app.listen(PORT, HOST);
